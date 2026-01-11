4017eff1285873b1e4a3b29866daada4b904b318
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-01-11 16:56:09 +0100

    Add Commands sample page for MVVM command bindings demo

---
+++ samples/AvaloniaDemo.Base/Views/CommandsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaDemo.Views;
+
+public partial class CommandsView : UserControl
+{
+    public CommandsView()
+    {
+        InitializeComponent();
+    }
+}
